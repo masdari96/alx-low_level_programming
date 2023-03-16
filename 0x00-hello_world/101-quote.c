566637650697d93daa0c219a0ecd409daf8a2f92
masdari96 <aminemasdari@gmail.com>
2023-03-16 16:03:26 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,10 @@
 #include <unistd.h>
 /**
  * main-Entry point
- *
- * Return: Always 1 (success).
+ * Return 1 (success)
  */
 int main(void)
 {
-      write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+      fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
       return (1);
 }
